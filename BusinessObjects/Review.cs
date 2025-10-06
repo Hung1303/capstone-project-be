85c0bcc1a5f9a1104561af2366ed2e9a086e19b6
Đỗ Tuấn Hùng <hungdtse172297@fpt.edu.vn>
2025-10-06 12:34:44 +0700

    Update db

--- BusinessObjects/Review.cs
+++ BusinessObjects/Review.cs
@@ -1,4 +1,6 @@
 using Core.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessObjects
 {
@@ -7,13 +9,22 @@ namespace BusinessObjects
         public Guid CourseId { get; set; }
         public Guid? StudentProfileId { get; set; }
         public Guid? ParentProfileId { get; set; }
+        [Range(1, 5)]
         public int Rating { get; set; }
+        [StringLength(4000)]
         public string Comment { get; set; } = string.Empty;
         public ReviewStatus Status { get; set; } = ReviewStatus.PendingModeration;
         public Guid? ModeratedByUserId { get; set; }
         public DateTimeOffset? ModeratedAt { get; set; }
+        [StringLength(500)]
         public string? ModerationNotes { get; set; }
+
+
+        public StudentProfile? StudentProfile { get; set; }
+        public ParentProfile? ParentProfile { get; set; }
+        public User? ModeratedByUser { get; set; }
     }
+
 }
 
 
