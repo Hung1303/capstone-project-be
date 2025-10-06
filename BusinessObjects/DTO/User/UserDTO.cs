7627dc070b1452a9a2a667112922d1a3fd9daa9b
Đỗ Tuấn Hùng <hungdtse172297@fpt.edu.vn>
2025-10-06 11:36:34 +0700

    UserDTO

--- BusinessObjects/DTO/User/UserDTO.cs
+++ BusinessObjects/DTO/User/UserDTO.cs
@@ -25,4 +25,43 @@
         public string Address { get; set; }
         
     }
+
+    public class CreateTeacherRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+        public int YearOfExperience { get; set; }
+        public string Qualifications { get; set; }
+        public string LicenseNumber { get; set; }
+        public string Subjects { get; set; }
+        public string Bio { get; set; }
+    }
+
+    public class CreateParentRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+        public string Address { get; set; }
+        public string PhoneSecondary { get; set; }
+    }
+
+    public class CreateStudentRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+        public string SchoolName { get; set; }
+        public string GradeLevel { get; set; }
+    }
 }
