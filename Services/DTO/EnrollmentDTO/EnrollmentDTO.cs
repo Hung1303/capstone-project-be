3a7b97fa3f158be9b7a86c8d09d9f06f305cc791
Đỗ Tuấn Hùng <hungdtse172297@fpt.edu.vn>
2025-12-07 00:58:42 +0700

    Add GET enrollments by student, Update GET APIs response to display more info

--- Services/DTO/EnrollmentDTO/EnrollmentDTO.cs
+++ Services/DTO/EnrollmentDTO/EnrollmentDTO.cs
@@ -20,6 +20,27 @@ namespace Services.DTO.EnrollmentDTO
     {
         public Guid Id { get; set; }
         public Guid CourseId { get; set; }
+        public string StudentName { get; set; }
+        public string ParentName { get; set; }
+        public string SchoolName { get; set; }
+        public int Gradelevel { get; set; }
+        public string Subject { get; set; }
+        public string TeachingMethod { get; set; }
+        public string Location { get; set; }
+        public Guid StudentProfileId { get; set; }
+        public EnrollmentStatus Status { get; set; }
+        public DateTimeOffset? ConfirmedAt { get; set; }
+        public DateTimeOffset? CancelledAt { get; set; }
+        public string? CancelReason { get; set; }
+    }
+
+    public class StudentEnrollmentResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public string Subject { get; set; }
+        public string TeachingMethod { get; set; }
+        public string Location { get; set; }
         public Guid StudentProfileId { get; set; }
         public EnrollmentStatus Status { get; set; }
         public DateTimeOffset? ConfirmedAt { get; set; }
