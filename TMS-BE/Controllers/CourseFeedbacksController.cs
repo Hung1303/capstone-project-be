4d3e884d953ff3dd460db3e5e2fafc555b6f2133
Đỗ Tuấn Hùng <hungdtse172297@fpt.edu.vn>
2025-12-09 14:26:50 +0700

    Add catch exception to UsersController, CenterVerificationController, FeedbackControllers

--- TMS-BE/Controllers/CourseFeedbacksController.cs
+++ TMS-BE/Controllers/CourseFeedbacksController.cs
@@ -21,78 +21,122 @@ namespace API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllCoursesFeedbacks([FromQuery] CourseFeedbackQuery query)
         {
-            var (feedbacks, totalCount) = await _courseFeedbackService.GetAllCoursesFeedbacks(query);
-            var response = new
+            try
             {
-                TotalCount = totalCount,
-                PageNumber = query.PageNumber,
-                PageSize = query.PageSize,
-                TotalPages = (int)Math.Ceiling(totalCount / (double)query.PageSize),
-                Data = feedbacks
-            };
+                var (feedbacks, totalCount) = await _courseFeedbackService.GetAllCoursesFeedbacks(query);
+                var response = new
+                {
+                    TotalCount = totalCount,
+                    PageNumber = query.PageNumber,
+                    PageSize = query.PageSize,
+                    TotalPages = (int)Math.Ceiling(totalCount / (double)query.PageSize),
+                    Data = feedbacks
+                };
 
-            return Ok(response);
+                return Ok(response);
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet("course/{courseId}")]
         public async Task<IActionResult> GetAllFeedbacksByCourse(Guid courseId, [FromQuery]int pageNumber =1, [FromQuery]int pageSize =5, [FromQuery]int? rating =null)
         {
-            var (items, count) = await _courseFeedbackService.GetAllFeedbackByCourse(courseId, pageNumber, pageSize, rating);
-            var response = new
+            try
             {
-                TotalCount = count,
-                PageNumber = pageNumber,
-                PageSize = pageSize,
-                TotalPages = (int)Math.Ceiling(count / (double)pageSize),
-                Data = items
-            };
-            return Ok(response);
+                var (items, count) = await _courseFeedbackService.GetAllFeedbackByCourse(courseId, pageNumber, pageSize, rating);
+                var response = new
+                {
+                    TotalCount = count,
+                    PageNumber = pageNumber,
+                    PageSize = pageSize,
+                    TotalPages = (int)Math.Ceiling(count / (double)pageSize),
+                    Data = items
+                };
+                return Ok(response);
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetCourseFeedbackById(Guid id)
         {
-            var result = await _courseFeedbackService.GetCourseFeedbackById(id);
-            if (result == null) return NotFound("Feedback not found.");
-            return Ok(result);
+            try
+            {
+                var result = await _courseFeedbackService.GetCourseFeedbackById(id);
+                if (result == null) return NotFound("Feedback not found.");
+                return Ok(result);
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpPost("{courseId}")]
         [Authorize(Policy = "ParentStudent")]
         public async Task<IActionResult> CreateCourseFeedback(Guid courseId, Guid reviewerId, CreateCourseFeedbackRequest request)
         {
-            var result = await _courseFeedbackService.CreateCourseFeedback(courseId, reviewerId, request);
-            if (result == null) return NotFound("Course not found");
+            try
+            {
+                var result = await _courseFeedbackService.CreateCourseFeedback(courseId, reviewerId, request);
+                if (result == null) return NotFound("Course not found");
 
-            return Ok(result);
+                return Ok(result);
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpPut("Approve/{feedbackId}")]
         [Authorize(Policy = "InspectionAccess")]
         public async Task<IActionResult> ApproveCourseFeedback(Guid feedbackId, Guid moderatorId, [FromBody] CourseFeedbackModerationRequest request)
         {
-            var result = await _courseFeedbackService.ApproveCourseFeedback(feedbackId, moderatorId, request);
+            try
+            {
+                var result = await _courseFeedbackService.ApproveCourseFeedback(feedbackId, moderatorId, request);
 
-            if (result == null) return NotFound("Feedback not found.");
-            return Ok(result);
+                if (result == null) return NotFound("Feedback not found.");
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpPut("{feedbackId}")]
         [Authorize(Policy = "ParentStudent")]
         public async Task<IActionResult> UpdateCourseFeedback(Guid feedbackId, [FromBody] UpdateCourseFeedbackRequest request)
         {
-            var result = await _courseFeedbackService.UpdateCourseFeedback(feedbackId, request);
-            if (result == null) return NotFound("Feedback not found.");
+            try
+            {
+                var result = await _courseFeedbackService.UpdateCourseFeedback(feedbackId, request);
+                if (result == null) return NotFound("Feedback not found.");
 
-            return Ok(result);
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveCourseFeedback(Guid id)
         {
-            var result = await _courseFeedbackService.RemoveCourseFeedback(id);
-            if (!result) return NotFound("Delete failed.");
-            return result ? Ok(new { message = "Delete successfully." }) : BadRequest();
+            try
+            {
+                var result = await _courseFeedbackService.RemoveCourseFeedback(id);
+                if (!result) return NotFound("Delete failed.");
+                return result ? Ok(new { message = "Delete successfully." }) : BadRequest();
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
